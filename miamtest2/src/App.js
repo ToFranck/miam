f72d82eaeaa533cc227a977c903bb5debfb392fd
Franck Tolosano <f.tolosano@nanaba.tech>
2022-04-22 15:37:46 +0200

    oui

--- miamtest2/src/App.js
+++ miamtest2/src/App.js
@@ -7,20 +7,18 @@ import { useEffect, useState } from "react";
 import { collection, onSnapshot } from 'firebase/firestore';
 
 
-// import Home from './components/Home';
-// import Commerces from './Commerces/Commerces';
+import Home from './components/Home';
+import Commerces from './Commerces/Commerces';
 
-// import {Routes, Route}from "react-router-dom";
-// import Profile from './components/Profile';
-// import Navbar from  './Navbar/Navbar';
-// import Boucheries from './Commerces/Boucheries';
-// import Boulangeries from './Commerces/Boulangeries';
+import {BrowserRouter as Router, Route, Routes}from "react-router-dom";
+import Profile from './components/Profile';
+import Navbar from  './Navbar/Navbar';
+import Boucheries from './Commerces/Boucheries';
+import Boulangeries from './Commerces/Boulangeries';
 
 
 function App() {
   const [users, setUsers] = useState([]);
-
-
   useEffect(
     () =>  
       onSnapshot(collection(db,"users"),(snapshot) => {
@@ -32,16 +30,28 @@ function App() {
   return (
    <div className='root'>
 
+    
+<Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/commerces" element={<Commerces/>}>
+          {/* imbriquer des root pour un coté plus fast */}
+          <Route path="/commerces/boulangeries" element={<Boulangeries/>} />
+          <Route path="/commerces/boucheries" element={<Boucheries/>} />
+        </Route>
+        <Route path="/profil" element={<Profile/>} />
+        <Route path="/profil/:name" element={<Profile/>} />
 
-     
-     <p>Hey 
+
+      </Routes>
+
+
+     {/* <p>Hey 
        {users.map(users => (
          <p> {users.name} </p>
         ) )}
-     </p>
+     </p> */}
 
-
-     
    </div>
   );
       
